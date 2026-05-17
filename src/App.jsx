7c93ee62d57a2456ea1012a1995767b62ce36727
Manjari <manjari@Manjari.localdomain>
2026-05-18 00:22:04 +0300

    added code to make timer auto update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,16 @@
 import { format } from "date-fns";
+import { useEffect, useState } from "react";
 
 function App() {
+  // const [CurrTime, setTime] = useState(new Date());
+
+  // useEffect(() => {
+  //   const timer = setInterval(() => {
+  //     setTime(new Date());
+  //   }, 1000);
+
+  //   return () => clearInterval(timer);
+  // }, []);
   return (
     <div className="container">
       <p>
